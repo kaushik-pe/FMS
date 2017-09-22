20ffe2445c854131950bf949c3aa7cd2be065777
Kaushik Kumaran <kaushikkumaran@icloud.com>
2017-09-22 19:49:11 +0530

    Filters completed

--- app/controllers/booking_controller.rb
+++ app/controllers/booking_controller.rb
@@ -4,30 +4,41 @@ class BookingController < ApplicationController
   def list
      dateQuery = "schedules."+Date.parse(params[:jrnydate]).strftime("%A").downcase
      unless params[:sortby]
-       @flights = Flight.joins(:schedule).where(:source=>params[:source],:destination=>params[:destination],dateQuery=>'t')
+       @flights = Flight.joins(:schedule).where(params.permit(:source,:destination,:airlines)).where(dateQuery=>"t")
        @flights = findPriceClass(@flights)
      end
      if params[:sortby]
        attribute = params[:sortby].to_sym
        if attribute==:price
-         @flights = Flight.joins(:schedule).where(:source=>params[:source],:destination=>params[:destination],dateQuery=>'t')
+         @flights = Flight.joins(:schedule).where(params.permit(:source,:destination,:airlines)).where(dateQuery=>"t")
          @flights = findPriceClass(@flights)
-         puts "Sorting by price"
          @flights = @flights.sort_by do |flight|
            flight.minPrice
          end
        else
-         @flights = Flight.joins(:schedule).where(:source=>params[:source],:destination=>params[:destination],dateQuery=>'t').order(attribute)
+         @flights = Flight.joins(:schedule).where(params.permit(:source,:destination,:airlines),dateQuery=>'t').order(attribute)
          @flights = findPriceClass(@flights)
        end
      end
-
+     if params[:minPrice] and params[:maxPrice]
+        @flights = @flights.find_all do |flight|
+            flight.minPrice <=  params[:maxPrice].to_i && params[:minPrice].to_i <= flight.maxPrice
+        end
+     end
+     if params[:minTime] and params[:maxTime]
+        @flights = @flights.find_all do |flight|
+            departureTemp = flight.departure_time.strftime('%H:%M')
+            puts "Time #{departureTemp}"
+            puts "MinTime #{params[:minTime]}"
+            #puts "Time #{arrivalTemp.class}"
+            #puts "MinTime #{params[:minTime].class}"
+            departureTemp >=  params[:minTime] && departureTemp <= params[:maxTime]
+        end
+     end
   end
 
   private
   def findPriceClass(flights)
-    puts "Length"
-    puts flights.length
     for i in 0..flights.length-1
         minPrice = Flight.joins(:schedule,:flighttype).where(:id=>flights[i].id).group("flights.id").minimum("flighttypes.price")
         maxPrice = Flight.joins(:schedule,:flighttype).where(:id=>flights[i].id).group("flights.id").maximum("flighttypes.price")

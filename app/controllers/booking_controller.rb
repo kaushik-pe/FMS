f1ee9adb98ac705460ba1d0207fe4812f9622461
Kaushik Kumaran <kaushikkumaran@icloud.com>
2017-09-26 17:18:25 +0530

    Avaiable seats are shown in results

--- app/controllers/booking_controller.rb
+++ app/controllers/booking_controller.rb
@@ -33,16 +33,57 @@ class BookingController < ApplicationController
             departureTemp >=  params[:minTime] && departureTemp <= params[:maxTime]
         end
      end
-  end
 
+     #removing flights without free seats#
+     @flights = @flights.find_all do |flight|
+       classes = Flighttype.where(:flight_id=>flight.id).pluck(:classname);
+       status = false;
+       classes.each do |classname|
+        total = findTotalSeats(flight.id,classname);
+        booked = findBookedSeats(flight.id,params[:jrnydate],classname)
+        if(booked.length<total)
+             status = true;
+         end
+       end
+       status;
+     end
+     #adding seats available for each class#
+     for i in 0..@flights.length-1 do
+       classes = Flighttype.where(:flight_id=>@flights[i].id).pluck(:classname);
+       status = false;
+       l = 0;
+       temp = {};
+       classes.each do |classname|
+         total = findTotalSeats(@flights[i].id,classname);
+         booked = findBookedSeats(@flights[i].id,params[:jrnydate],classname)
+         temp[classname] = total-booked.length;
+       end
+       @flights[i].availableSeatMap = temp;
+     end
+  end
   def book
     if !@user
       redirect_to '/'
     end
     @flightClasses = Flighttype.joins(:flight).where(:flight_id=>params[:flight_id]).pluck(:classname)
+    #filter classes without available seats
+    @availableSeats = [];
+    @flightClasses = @flightClasses.find_all do |classname|
+      total = findTotalSeats(params[:flight_id],classname);
+      booked = findBookedSeats(params[:flight_id],params[:jrnydate],classname)
+      if(booked.length<total)
+           status = true;
+           @availableSeats<<(total-booked.length);
+      end
+      status
+    end
+    @availbleJavastr = "";
     @flightJavaStr = ""
     @flightClasses.each do|flightClass|
       @flightJavaStr +=flightClass.to_s+","
+    end
+    @availableSeats.each do |availableSeats|
+      @availbleJavastr+=availableSeats.to_s+","
     end
   end
 
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,8 @@
 class Flight < ApplicationRecord
+  @availableSeatMap = {}
   has_one :schedule
   has_many :flighttype
   has_many :booking
   attr_accessor :maxPrice,:minPrice
+  attr_accessor :availableSeatMap
 end
